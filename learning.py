700783a8fda3d086e5f871579e99e91506ac3861
jrai-tech <292438549+jrai-tech@users.noreply.github.com>
2026-06-10 22:26:10 +0100

    add password generator project

--- learning.py
+++ learning.py
@@ -1,1 +1,19 @@
 print("First line of code")
+
+
+# Password Generator
+# My first project
+
+import random #Lets the code pick random things
+
+print("Password: ")
+
+#Gives random characters to choose from
+characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!?@#" 
+
+password = "" #To start with an empty password
+
+for x in range (24): #Repeats it 24 times
+    password += random.choice(characters) #Adds a random character from "characters"
+
+print(password) #Prints the final password below "Password: "
